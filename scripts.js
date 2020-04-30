f0396e209c49fcc055c8e213b2e0cabc6b7867d1
arpado <64147632+arpado@users.noreply.github.com>
2020-04-30 15:04:46 +0200

    continue
    - thumbnails with click function added

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ data = {
 };*/
 
 //A képeket és a szövegeket generáló rész
+
 var photo = ['img/Forest-fire.jpg', 'img/AuroraClouds_Boffelli_2048.jpg', 'img/Will_o_the_wisp_by_fladdermuuus.jpg','img/Tulips.jpg','img/Desert.jpg','img/Hydrangeas.jpg','img/Backyard.jpg','img/Golfcourse.jpg', 'img/Rainy-street.jpg', 'img/Wimbledon-station.jpg', 'img/Londawn.jpg'];
 var title = ['Tűz', 'Auróra', 'Will-o-Wisp', 'Tulipánok', 'Sivatag, kaktuszok nélkül', 'Virág (?)', 'Londoni Hátsókert, cc. 2017', 'Wimbledon Golfcourse', 'Esős londoni utca', 'Wimbledon Station', 'Londawn'];
 var description = ['Ez itt egy lángoló erdő.', 'Ez itt az Aurora Borealis.', 'Ez pedig egy tündérfény.', 'Önmagáért beszél.', 'Sivatag, kaktusz nélkül.', 'Fogalmam sincs, milyen virág ez, itt volt a mappában, és egész jól néz ki.', '"...és ez itten a kert..."', 'Golfpálya, nahát. Szép hely amúgy.', 'Ez pedig a golfpálya mögötti út. A házakat úgy nézd, hogy sem te, sem én nem veszünk itt levegőn kívül soha semmit.', 'Metrómegálló a város szélén.', 'O2 Aréna, egy december reggel. Élőben szebb volt.'];
@@ -29,9 +30,6 @@ $('#button-right').click(() => {
         currentPhoto = -1};
     currentPhoto++;
     loadPhoto(currentPhoto);
-  
-  
-
 });
 
 //valamiért nem jó a visszafelékattintásnál a ciklus, kettőt kell kattintani, de nem értem, miért
@@ -41,3 +39,24 @@ $('#button-left').click(() => {
     currentPhoto--;
     loadPhoto(currentPhoto);
 });
+
+// A thumbnailek generálásáért felelős rész
+
+photo.forEach((item, index) => {
+$('#thumbnail-box').append(`<div class="thumbnail"> <img id="thumbnail-image" src="${item}" photoindex="${index}"> </div>`);
+
+//A thumbnailek klikk-reagálása
+    
+    $('.thumbnail').click((event) => {
+        let photoindex2 = $(event.target).attr('photoindex');
+        let photoindex3 = parseInt(photoindex2);
+        //console.log(photoindex3);
+        loadPhoto(photoindex3);
+        currentPhoto = (photoindex3)
+    });
+    
+});
+
+
+
+
